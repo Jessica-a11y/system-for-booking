8e4ae563d1706cc8e73e983a00fcbe7fab268d76
Jessica <olofsson.jessica@hotmail.com>
2025-05-08 13:29:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="employee-overview">
+    <img src="./assets/user.png" alt="User imag" />
+    <div>
+      <span class="employee-profession">Plumer</span><br>
+      <span class="employee-name">Maria</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.employee-overview {
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+}
+
+.employee-overview > img {
+    width: 34px;
+    height: 34px;
+    margin-inline: 0.5rem;
+}
+
+.employee-profession {
+    font-size:small;
+}
+.employee-name {
+    font-weight: bold;
+}</style>
