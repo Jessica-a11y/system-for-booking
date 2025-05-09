b740dfcde46878869fba20b591b171addea0ccd3
Jessica <olofsson.jessica@hotmail.com>
2025-05-09 08:07:14 +0200

    adding header to webpage

--- src/App.vue
+++ src/App.vue
@@ -23,15 +23,36 @@ import avatar from './assets/cartoon-picture.png';
 </script>
 
 <template>
+  <div class="header">
+            <h1>Svenssons Hantverk AB</h1>
+        </div>
+  <EmployeeCard :avatar="avatar" profession="Snickare" name="Joakim Ihlström" status="Bokad"
+    date="2025-04-30 (Ons)" time="08.00–16.00" workType="Snickare" workplace="Mölndals Kommun -Byggplats A"
+    contact="031-88 66 55" />
+
   <div class="employee-overview">
     <EmployeeTag  v-for="em in employees" :employeeImg="employeeImg" :name="em.name" :professions="em.professions"></EmployeeTag>
   </div>
 
-  <EmployeeCard :avatar="avatar" profession="Snickare" name="Joakim Ihlström" status="Bokad"
-    date="2025-04-30 (Ons)" time="08.00–16.00" workType="Snickare" workplace="Mölndals Kommun -Byggplats A"
-    contact="031-88 66 55" />
+  
 </template>
 
 <style scoped>
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.header{
+    background-color: #434446;
+    color:#CFCFCF;
+    margin: 0;
+}
+
+h1 {
+    margin:0;
+    font-size: large;
+    padding-block: 0.7rem;
+    padding-left: 1.5rem;
+}
 
 </style>
